3dd69c42048cec107095c25ef1668fb3fbf5a878
psofiterol <gpentafragkas@protonmail.com>
2026-01-22 13:08:02 +0200

    fix: nilcc config

--- scripts/measurement-hash.sh
+++ scripts/measurement-hash.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 SCRIPT_PATH=$(dirname $(realpath $0))
 ROOT_PATH="$SCRIPT_PATH/../"
-DOCKER_COMPOSE_HASH=1d371953fa04141c6385380bde4e9074689def05c4cf9b0d023c737dec278d8c
-VCPUS=1
+DOCKER_COMPOSE_HASH=$(sha256sum $ROOT_PATH/docker-compose-nilcc.yaml | cut -d" " -f1)
+VCPUS=4
 NILCC_VERSION=0.3.0
 NILCC_VERIFIER_VERSION=0.4.0
 
